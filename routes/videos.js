07764f4f82e6e8c62bf789bad4d784f14fecffa0
ayodyapraneeth <ayodya.praneeth@gmail.com>
2023-07-10 19:28:02 -0700

    Finish my API

--- routes/videos.js
+++ routes/videos.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const fs = require("fs");
+const path = require("path");
+const { v4: uuidv4 } = require("uuid"); // Import the uuid package
+const videosFilePath = path.join(__dirname, "../data/videos.json");
 
 const videos = require("../data/videos.json");
+const { channel } = require("diagnostics_channel");
 
 router.route("/").get((req, res) => {
 	const filteredVideos = videos.map((video) => {
@@ -15,10 +20,11 @@ router.route("/").get((req, res) => {
 router.route("/:id").get((req, res) => {
 	const videoId = req.params.id;
 
+	
 	const foundVideo = videos.find((video) => video.id === videoId);
 
 	if (foundVideo) {
-
+		
 		const {
 			id,
 			title,
@@ -58,6 +64,56 @@ router.route("/:id").get((req, res) => {
 		
 		res.status(404).json({ error: "Video not found" });
 	}
+});
+
+router.route("/").post((req, res) => {
+	const { title, description, channel, image, video } = req.body;
+	console.log("Received data:", req.body);
+
+	
+	const newVideoId = uuidv4();
+
+	
+	const newCommentId = uuidv4();
+
+	
+	const newComment = {
+		id: newCommentId,
+		name: "Maheshi",
+		comment:
+			"Wow, this video is absolutely captivating! The visuals are stunning, and the information presented is both informative and thought-provoking. I appreciate how well it's edited and how engaging the narration is. It kept me hooked from start to finish. Great job to everyone involved in creating this fantastic piece of content!",
+		likes: 5,
+		timestamp: Date.now(),
+	};
+
+
+	const newVideo = {
+		id: newVideoId,
+		title: title,
+		channel: channel,
+		image: image,
+		description: description,
+		views: "0",
+		likes: "0",
+		duration: "0:00",
+		video: video,
+		timestamp: Date.now(),
+		comments: [newComment],
+	};
+
+	
+	videos.push(newVideo);
+
+
+	fs.writeFile(videosFilePath, JSON.stringify(videos), (err) => {
+		if (err) {
+			console.error("Error writing videos file:", err);
+			res.status(500).json({ error: "Internal Server Error" });
+		} else {
+			
+			res.status(201).json(newVideo);
+		}
+	});
 });
 
 module.exports = router;
